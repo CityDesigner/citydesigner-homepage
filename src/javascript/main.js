66b34e4ffe558bb0fddc34e266b6bcb1dbd5d136
Ilja <iljapanic@gmail.com>
2017-04-26 00:53:16 +0200

    improve slider design

---
+++ src/javascript/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  $('.slider-author').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-quote',
+    dots: false,
+    focusOnSelect: true,
+    centerMode: true,
+    arrows: false,
+    variableWidth: true,
+    infinite: true
+  });
+
+  $('.slider-quote').slick({
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6500,
+    fade: true,
+    arrows: false,
+    asNavFor: '.slider-author'
+  });
+
+});
